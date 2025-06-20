470f804307e2c3377d6342591409981eb673870e
holdn2 <yoocy01@gmail.com>
2025-06-20 17:18:14 +0900

    feat : 섹션 6. state관련 실습 완료

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -1,24 +1,19 @@
+// 그냥 let 같은 변수가 아닌 state로 저장하는 이유는?
+// state여야지 값이 바뀔 때마다 리렌더링 할 수 있다.
+// props로 준 값이 바껴도 리렌더링된다.
+
+// 리렌더링 되는 조건
+// 1. 자신이 가진 state가 변경되었을 때
+// 2. 받은 props의 값이 변경되었을 때
+// 3. 부모 컴포넌트가 리렌더링되었을 때
+
 import "./App.css";
-import Button from "./components/Button";
-import Footer from "./components/Footer";
-import Header from "./components/Header";
-import Main from "./components/Main";
+import Register from "./components/Register";
 
 function App() {
-  const buttonProps = {
-    text: "메일",
-    color: "red",
-    a: 1,
-    b: 2,
-    c: 3,
-  };
   return (
     <>
-      <Button {...buttonProps} />
-      <Button text={"카페"} />
-      <Button text={"블로그"}>
-        <div>자식 요소</div>
-      </Button>
+      <Register />
     </>
   );
 }
