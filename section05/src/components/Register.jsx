8e2edcf8c4bca092bd8089df148cc406ed695842
holdn2 <yoocy01@gmail.com>
2025-06-20 17:27:10 +0900

    feat : useRef 실습 / 조금 더 공부가 필요한 부분인듯

--- section05/src/components/Register.jsx
+++ section05/src/components/Register.jsx
@@ -4,7 +4,7 @@
 // 3. 국적
 // 4. 자기소개
 
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 const Register = () => {
   const [input, setInput] = useState({
@@ -14,17 +14,32 @@ const Register = () => {
     bio: "",
   });
 
+  // 값이 변경되어도 리렌더링 되지 않는 변수를 만들고 싶을 때 useRef 사용
+  // dom 요소에 적용 가능함
+  const countRef = useRef(0);
+  const inputRef = useRef();
+
   const onChange = (e) => {
+    countRef.current++;
+    console.log(countRef.current);
     setInput({
       ...input,
       [e.target.name]: e.target.value,
     });
   };
 
+  const onSubmit = () => {
+    if (input.name === "") {
+      // 이름을 입력하는 DOM 요소 포커스
+      inputRef.current.focus();
+    }
+  };
+
   return (
     <div>
       <div>
         <input
+          ref={inputRef}
           name="name"
           value={input.name}
           onChange={onChange}
@@ -50,6 +65,7 @@ const Register = () => {
       <div>
         <textarea name="bio" value={input.bio} onChange={onChange} />
       </div>
+      <button onClick={onSubmit}>제출</button>
     </div>
   );
 };
